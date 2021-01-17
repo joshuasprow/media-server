c3293992cd7f899e0fb5de6624f1eae489218f02
Joshua Sprow <jsprow@gmail.com>
2021-01-17 20:14:52 +0000

    replacing transmission with deluge. it's got finer-grained control over torrent lifecycle. also adding an extract script.

---
+++ scripts/extract.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# set this script to execute when a torrent download completes. it will 
+# automatically extract any .zip or .rar files into the torrent's download dir.
+
+formats=(zip rar)
+commands=([zip]="unzip -u" [rar]="unrar -o- e")
+extraction_subdir='extracted'
+
+torrentid=$1
+torrentname=$2
+torrentpath=$3
+
+log()
+{
+    logger -t deluge-extractarchives "$@"
+}
+
+log "Torrent complete: $@"
+
+cd "${torrentpath}"
+
+for format in "${formats[@]}"; do
+    while read file; do 
+        log "Extracting \"$file\""
+
+        cd "$(dirname "$file")"
+
+        file=$(basename "$file")
+
+        # if extraction_subdir is not empty, extract to subdirectory
+        if [[ ! -z "$extraction_subdir" ]] ; then
+            mkdir "$extraction_subdir"
+
+            cd "$extraction_subdir"
+
+            file="../$file"
+        fi
+
+        ${commands[$format]} "$file"
+
+    done < <(find "$torrentpath/$torrentname" -iname "*.${format}" )
+done
